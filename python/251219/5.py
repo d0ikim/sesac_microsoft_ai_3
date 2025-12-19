0cfec21746db86d6dabd155b93d5119e8b4e20ec
Doi Kim <doyclub@naver.com>
2025-12-19 15:38:03 +0900

    251219금(5) - 결정 제어 구조 합치기
    코드 컴팩트하게 압축하는 연습

---
+++ python/251219/5.py
@@ -0,0 +1,14 @@
+"""a = int(input())
+b = int(input())
+y = 0
+
+if a>0:
+    y+=1
+else:
+    print("안녕 철수!")
+    
+if y>0:
+    print(y+5)
+y+=1    # 두 번째 if문을 위한 y 증가
+if y<=0:
+    print(y+12)"""
