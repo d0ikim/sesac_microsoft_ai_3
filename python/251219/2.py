7df84e95834e9944083d9e94c2efc27a18128a97
Doi Kim <doyclub@naver.com>
2025-12-19 11:24:36 +0900

    251219금(2) - 제어 구조
    - 복합 연산의 진리표(연산 우선순위 연습)
    - if문
    - if-else문

---
+++ python/251219/2.py
@@ -0,0 +1,23 @@
+"""a = 1
+b = -5
+c = 7
+print(a>2 or c>b and c>2)   # True
+print(not(a>2 or c>b and c>2))  # False
+print(a>2 or c>b and c>2)   # True
+print(not(a>2 or c>b and c>2))  # False  """
+
+"""x = int(input())
+if x<0:
+    # x = -x    # 음수를 양수로 바꾸기
+    # x *= -1   # 음수를 양수로 바꾸기
+    x = abs(x)    # abs() 함수를 사용하여 음수를 양수로 바꾸기
+print(x)"""
+
+"""a = float(input())
+b = float(input())
+if b>a:
+    big = b
+else:
+    big = a
+print(big)"""
+
