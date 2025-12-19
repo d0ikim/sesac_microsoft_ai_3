23ca4827d4192438dd9ff8b2d7aa807a91bb1ec0
Doi Kim <doyclub@naver.com>
2025-12-19 14:30:41 +0900

    251219금(4) - 중첩if문

---
+++ python/251219/4.py
@@ -0,0 +1,27 @@
+"""a = int(input())
+y = a*2
+if a>0:
+    x = y*300
+else:
+    x=y*100
+print(x)"""
+
+"""a = int(input())
+if a>0:
+    y=a*4
+else:
+    y=a*3
+print(y)"""
+
+"""a = float(input())
+if a>=30:
+    print("잘못된 숫자")
+else:
+    if a<10:
+        y=a*2
+    elif a<20:
+        y=a/2
+    else:
+        y=a+2
+    print(y)
+print("종료!")"""
